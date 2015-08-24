c0294e7835ec1ae328b855d2cf3f45696aece039
Ivy <ivywilson@usa.com>
2015-08-24 13:35:00 -0500

    Add player controls Create screen boundaries

--- js/app.js
+++ js/app.js
@@ -1,10 +1,25 @@
+var screenPixelsLeft = 1;
+var screenPixelsRight = 500; 
+var screenPixelsTop = 5;
+var screenPixelsBottom = 420;
+
+
+var numEnemies = 3;
+function randomInt(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x,y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+    this.speed = 5;
+    this.x = x;
+    this.y = y;
+    this.direction = "right";
     this.sprite = 'images/enemy-bug.png';
 }
 
@@ -14,6 +29,20 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if (this.direction === "right"){
+        this.x+=this.speed*dt;
+        if (this.x > screenPixelsRight){
+            this.direction = "left";           
+           // this.sprite = 'images/enemy-bug-left.png';
+       };
+    };
+       if (this.direction === "left"){        
+       this.x-=this.speed*dt;
+        if (this.x < screenPixelsLeft){
+            this.direction = "right"; 
+            this.sprite = 'images/enemy-bug.png';          
+        };
+    };
 }
 
 // Draw the enemy on the screen, required method for game
@@ -24,12 +53,51 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
-
+var Player = function(){
+    this.x = 200;
+    this.y = screenPixelsBottom;
+    this.speed = 20;
+    this.sprite = 'images/char-boy.png';
+};
+Player.prototype = Object.create(Enemy.prototype);
+Player.prototype.constructor = Player;
+Player.prototype.update = function(){
+    if (this.x<screenPixelsLeft){
+        this.x++;
+    };
+    if (this.x>screenPixelsRight){
+        this.x--;
+    };
+    if (this.y>screenPixelsBottom){
+        this.y--;
+    };
+    if (this.y<screenPixelsTop){
+    //you win
+    this.y = screenPixelsBottom;
+    };
+};
+Player.prototype.handleInput = function(key){
+    if (key === "up"){
+        this.y-= this.speed;
+    };
+    if (key === "down"){
+        this.y+= this.speed;
+    };
+    if (key === "left"){
+        this.x-= this.speed;
+    };
+    if (key === "right"){
+        this.x+= this.speed;
+    };
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [];
+for (i = 0; i<numEnemies; i++){
+allEnemies.push(new Enemy(randomInt(screenPixelsLeft, screenPixelsRight), randomInt(screenPixelsTop, screenPixelsBottom - 100)));
+}
+var player = new Player;
 
 
 // This listens for key presses and sends the keys to your
